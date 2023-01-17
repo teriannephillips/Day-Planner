3e406cdb999678cfbfa584491d3c1dbd80334ed4
Teri Phillips <phillips.terianne@gmail.com>
2023-01-17 14:30:52 -0500

    apply classes, event listener, and saved data

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -4,32 +4,57 @@
 
 $(function () {
 
+// Display the current date in the header of the page.
+let now = dayjs();
+let date = now.format("dddd, MMMM D YYYY");
+var dateEl = document.getElementById("currentDay");
+dateEl.innerHTML = date;
 
+//formatted the time from Day.js to get current hour
+var time = now.format("HH");
+//apply past, present, or future class to each time block 
+for (var i = 9; i < 18; i++) {
+  var hour = "hour-" + i;
+  var timeEl = document.getElementById(hour);
+  if (i < time) {
+    timeEl.className = "row time-block past"
+  }
+  else if (i > time) {
+    timeEl.className = "row time-block future"
+  }
+  else {
+    timeEl.className = "row time-block present";
+  }
+}
+//add event listener for when a button is clicked
+document.addEventListener("click", function (event) {
+  if (event.target.matches("button")) {
+    var button = event.target;
+    var getBlock = button.closest(".row");
+    var getHour = getBlock.getAttribute("id");
+    var index = getHour.split('-')[1];
+    console.log(index);
+    var textId = "text-" + index;
+    var data = document.getElementById(textId).value
+    saveData(data, index);
+  }
+});
+//saves the data to local storage
+var saveData = function (data, index) {
+  var calendarData = JSON.parse(localStorage.getItem('calendarData')) || ["", "", "","", "", "","", "", "",];
+  calendarData.splice(index-9,1, data);
+  localStorage.setItem("calendarData", JSON.stringify(calendarData));
+}
+//shows the saved data on the screen even after refresh
+var renderData = function () {
+  var storedData= JSON.parse(localStorage.getItem('calendarData')) || ["", "", "","", "", "","", "", "",];
+  for (var i = 0; i <9; i++){
+    index = i + 9;
+    textId = "text-" + index
+    var dataEl = document.getElementById(textId);
+    dataEl.value = storedData[i];
+  }
+}
+renderData();
 
-
-
-
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // Display the current date in the header of the page.
-  let now = dayjs();
-  console.log();
-  let date = now.format("dddd, MMMM D YYYY");
-  var dateEl = document.getElementById("currentDay");
-  dateEl.innerHTML = date;
 });
