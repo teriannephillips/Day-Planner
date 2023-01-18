55cc815ef8fbc670ef82d7d6c07c2867d9ea9e20
Teri Phillips <phillips.terianne@gmail.com>
2023-01-18 08:02:54 -0500

    saved data by date

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -4,54 +4,58 @@
 
 $(function () {
 
-// Display the current date in the header of the page.
-let now = dayjs();
-let date = now.format("dddd, MMMM D YYYY");
-var dateEl = $("#currentDay");
-dateEl.html(date);
+  // Display the current date in the header of the page.
+  let now = dayjs();
+  let date = now.format("dddd, MMMM D YYYY");
+  var dateEl = $("#currentDay");
+  dateEl.html(date);
 
-//formatted the time from Day.js to get current hour
-var time = now.format("HH");
-//apply past, present, or future class to each time block 
-for (var i = 9; i < 18; i++) {
-  var hour = "hour-" + i;
-  var timeEl = $("#" + hour);
-  if (i < time) {
-    timeEl.attr("class", "row time-block past");
+  //formatted the time from Day.js to get current hour
+  var time = now.format("HH");
+  //apply past, present, or future class to each time block 
+  for (var i = 9; i < 18; i++) {
+    var hour = "hour-" + i;
+    var timeEl = $("#" + hour);
+    if (i < time) {
+      timeEl.attr("class", "row time-block past");
+    }
+    else if (i > time) {
+      timeEl.attr("class", "row time-block future");
+    }
+    else {
+      timeEl.attr("class", "row time-block present");
+    }
   }
-  else if (i > time) {
-    timeEl.attr("class", "row time-block future");
+  //add event listener for when a button is clicked
+  $(document).on("click", "button", function (event) {
+    var button = $(this);
+    var getBlock = button.closest(".row");
+    var getHour = getBlock.attr("id");
+    var index = getHour.split('-')[1];
+    var textId = "text-" + index;
+    var data = $("#" + textId).val();
+    saveData(data, index);
+  });
+  //saves the data to local storage using the date so each day starts fresh
+  var saveData = function (data, index) {
+    let now = dayjs();
+    let today = now.format();
+    var date = today.split('T')[0];
+    console.log(date);
+    var calendarData = JSON.parse(localStorage.getItem(date)) || ["", "", "", "", "", "", "", "", "",];
+    calendarData.splice(index - 9, 1, data);
+    localStorage.setItem(date, JSON.stringify(calendarData));
   }
-  else {
-    timeEl.attr("class", "row time-block present");
+  //shows the saved data on the screen even after refresh
+  var renderData = function () {
+    var storedData = JSON.parse(localStorage.getItem(date)) || ["", "", "", "", "", "", "", "", "",];
+    for (var i = 0; i < 9; i++) {
+      index = i + 9;
+      textId = "text-" + index
+      var dataEl = document.getElementById(textId);
+      dataEl.value = storedData[i];
+    }
   }
-}
-//add event listener for when a button is clicked
-$(document).on("click", "button", function (event) {
-  var button = $(this);
-  var getBlock = button.closest(".row");
-  var getHour = getBlock.attr("id");
-  var index = getHour.split('-')[1];
-  var textId = "text-" + index;
-  var data = $("#" + textId).val();
-  saveData(data, index);
-});
-//saves the data to local storage
-var saveData = function (data, index) {
-  var calendarData = JSON.parse(localStorage.getItem('calendarData')) || ["", "", "","", "", "","", "", "",];
-  calendarData.splice(index-9,1, data);
-  localStorage.setItem("calendarData", JSON.stringify(calendarData));
-}
-//shows the saved data on the screen even after refresh
-var renderData = function () {
-  var storedData= JSON.parse(localStorage.getItem('calendarData')) || ["", "", "","", "", "","", "", "",];
-  for (var i = 0; i <9; i++){
-    index = i + 9;
-    textId = "text-" + index
-    var dataEl = document.getElementById(textId);
-    dataEl.value = storedData[i];
-  }
-}
-renderData();
+  renderData();
 
 });
